dc88f15cd39fe987bc18064973db5ae99bf08928
廖荣杰 <liaorongjie@huize.com>
2022-07-04 17:48:27 +0800

    optimize styles, add lspconfig

--- chadrc.lua
+++ chadrc.lua
@@ -3,18 +3,25 @@ local M = {}
 local override = require "custom.override"
 
 M.ui = {
-   statusline = {
-      separator_style = "arrow",
-   },
+    statusline = {
+        separator_style = "arrow",
+    },
 }
 
 M.plugins = {
-   user = require "custom.plugins",
+    user = require "custom.plugins",
 
-   override = {
-      ["hrsh7th/nvim-cmp"] = override.cmp,
-      ["lewis6991/gitsigns.nvim"] = override.gitsigns,
-   },
+    override = {
+        ["hrsh7th/nvim-cmp"] = override.cmp,
+        ["lewis6991/gitsigns.nvim"] = override.gitsigns,
+        ["nvim-treesitter/nvim-treesitter"] = override.treesitter,
+    },
+
+    options = {
+        lspconfig = {
+            setup_lspconf = "custom.plugins.lspconfig",
+        },
+    },
 }
 
 return M
--- override.lua
+++ override.lua
@@ -3,40 +3,53 @@ local M = {}
 local lspkind = require("lspkind")
 
 M.cmp = {
-   sources = {
-      { name = "cmp_tabnine" },
-      { name = "luasnip" },
-      { name = "nvim_lua" },
-      { name = "buffer" },
-      { name = "path" },
-   },
-   formatting = {
-      format = lspkind.cmp_format({
-         mode = "symbol_text",
-         maxwidth = 120,
-         before = function(entry, vim_item)
-            local kind = vim_item.kind
-            vim_item.kind = lspkind.presets.default[vim_item.kind]
-            local menu = "[" .. kind .. "]"
-            if entry.source.name == "cmp_tabnine" then
-               menu = "[TabNine]"
-               if entry.completion_item.data ~= nil and entry.completion_item.data.detail ~= nil then
-                  menu = menu .. " " ..entry.completion_item.data.detail
-               end
-               vim_item.kind = ""
-            end
+    sources = {
+        { name = "cmp_tabnine" },
+        { name = "luasnip" },
+        { name = "nvim_lua" },
+        { name = "buffer" },
+        { name = "path" },
+    },
+    formatting = {
+        format = lspkind.cmp_format({
+            mode = "symbol_text",
+            maxwidth = 120,
+            before = function(entry, vim_item)
+                local kind = vim_item.kind
+                vim_item.kind = lspkind.presets.default[vim_item.kind]
+                local menu = "[" .. kind .. "]"
+                if entry.source.name == "cmp_tabnine" then
+                    menu = "[TabNine]"
+                    if entry.completion_item.data ~= nil and entry.completion_item.data.detail ~= nil then
+                        menu = menu .. " " ..entry.completion_item.data.detail
+                    end
+                vim_item.kind = ""
+                end
 
-            vim_item.menu = menu
+                vim_item.menu = menu
 
-            return vim_item
-         end,
-      }),
-   },
+                return vim_item
+                end,
+        }),
+    },
 }
 
 M.gitsigns = {
-   current_line_blame = true,
-   current_line_blame_formatter = "    <author>, <author_time:%Y-%m-%d> - <summary>"
+    current_line_blame = true,
+    current_line_blame_formatter = "    <author>, <author_time:%Y-%m-%d> - <summary>"
+}
+
+M.treesitter = {
+    ensure_installed = {
+        "html",
+        "css",
+        "scss",
+        "python",
+        "javascript",
+        "typescript",
+        "vue",
+        "go",
+    }
 }
 
 return M
